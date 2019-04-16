e03b863c260b6c9dd210298c4e78a85e4fea21b7
Michael Li <mcli.893@gmail.com>
2019-04-15 18:17:20 -0600

    add gulp-html-partial plugin to create index.html from partials

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,48 @@
-const gulp = require('gulp');
-const clean = require('gulp-clean');
-const log = require('gulplog');
+const gulp = require('gulp')
+const clean = require('gulp-clean')
+const log = require('gulplog')
+// html-partial dependencies
+const htmlPartial = require('gulp-html-partial')
 // sass dependencies
-const sass = require('gulp-sass');
-sass.compiler = require('node-sass');
-const cleanCSS = require('gulp-clean-css');
+const sass = require('gulp-sass')
+sass.compiler = require('node-sass')
+const cleanCSS = require('gulp-clean-css')
 // js dependencies
-const babel = require('gulp-babel');
-const browserify = require('browserify');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
-const babelify = require('babelify');
+const babel = require('gulp-babel')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
+const uglify = require('gulp-uglify')
+const sourcemaps = require('gulp-sourcemaps')
+const babelify = require('babelify')
 
 // clean output folder
-gulp.task('clean', () => {
-  return gulp.src('./docs', {read: false})
+gulp.task('clean', ()=>{
+  return gulp.src('docs', {read: false})
     .pipe(clean());
 })
 
+// html partial compiler
+gulp.task('html', ()=>{
+  return gulp.src('src/html/index.html')
+    .pipe(htmlPartial({
+      basePath: 'src/html/partials/'
+    }))
+    .pipe(gulp.dest('docs'))
+})
+
 // sass bundler
-gulp.task('sass', () => {
+gulp.task('sass', ()=>{
   return gulp.src('src/sass/bundle.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS({debug: true}, details => {
-      console.log(`${details.name}: ${details.stats.originalSize} => ${details.stats.minifiedSize}`);
+      console.log(`${details.name}: ${details.stats.originalSize} ==minify=> ${details.stats.minifiedSize}`);
     }))
-    .pipe(gulp.dest('docs'));
-});
+    .pipe(gulp.dest('docs'))
+})
 
 // javascript browserify, babelify, minify
-gulp.task('javascript', () => {
+gulp.task('javascript', ()=>{
   var b = browserify({
     entries: 'src/js/main.js',
     debug: true,
@@ -46,12 +57,13 @@ gulp.task('javascript', () => {
     .pipe(uglify())
     .on('error', log.error)
     .pipe(sourcemaps.write('maps'))
-    .pipe(gulp.dest('docs'));
-});
+    .pipe(gulp.dest('docs'))
+})
 
-// create task
+// default task
 gulp.task('default', gulp.series(
   'clean',
+  'html',
   'sass',
   'javascript'
-));
+))
