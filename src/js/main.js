e808badad23a6e1f1c103b1df091f0c966a9a3ed
Michael Li <mcli.893@gmail.com>
2019-04-26 16:09:32 -0600

    add nav button visual js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,27 @@
 const Particles = require('particlesjs')
 
-Particles.init({
-  selector: '#particles',
-  maxParticles: 150,
-  speed: 0.25,
-  color: '#67daff',
-  minDistance: 30,
-  connectParticles: true
-});
+if (document.getElementById('particles')) {
+  Particles.init({
+    selector: '#particles',
+    maxParticles: 150,
+    speed: 0.25,
+    color: '#67daff',
+    minDistance: 30,
+    connectParticles: true
+  });
+}
+
+// nav button function
+const navButtons = Array.from(document.querySelectorAll('.nav__button'));
+if (navButtons.length > 0) {
+  navButtons.forEach(elem => {
+    elem.onclick = e => {
+      if (e.target.classList.contains('nav__button')) {
+        navButtons.forEach(button => {
+          button.classList.remove('selected');
+        });
+        e.target.classList.add('selected');
+      }
+    }
+  })
+}
