95b4e30692e5ce5ca1a767e6f0acea4a06d24d5c
Michael Li <mcli.893@gmail.com>
2019-04-27 14:45:08 -0600

    add about section layout style

--- src/js/main.js
+++ src/js/main.js
@@ -11,16 +11,43 @@ if (document.getElementById('particles')) {
   });
 }
 
+// change view function
+function changeView(view) {
+  switch(view){
+    case 'about':
+      viewAbout();
+      break;
+    case 'portfolio':
+      viewPortfolio();
+      break;
+    case 'contact':
+      viewContact();
+      break;
+    default:
+      return;
+  }
+  function viewAbout(){
+    console.log('view:about');
+  }
+  function viewPortfolio(){
+    console.log('view:portfolio');
+  }
+  function viewContact(){
+    console.log('view:contact');
+  }
+}
+
 // nav button function
 const navButtons = Array.from(document.querySelectorAll('.nav__button'));
 if (navButtons.length > 0) {
   navButtons.forEach(elem => {
     elem.onclick = e => {
-      if (e.target.classList.contains('nav__button')) {
+      if (e.target.classList.contains('nav__button') && !e.target.classList.contains('selected')) {
         navButtons.forEach(button => {
           button.classList.remove('selected');
         });
         e.target.classList.add('selected');
+        changeView(e.target.dataset.view);
       }
     }
   })
