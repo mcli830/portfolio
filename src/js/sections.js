512723d96a202eef212d41c108271c8bb8fb7088
mcli830 <39263631+mcli830@users.noreply.github.com>
2019-06-01 13:17:15 -0700

    change section headers to flat design, update body padding offset

--- src/js/sections.js
+++ src/js/sections.js
@@ -9,7 +9,7 @@ module.exports = function(animations) {
     var prev = document.querySelector('.portfolio__header.selected');
     prev.classList.remove('selected');
     this.className = 'portfolio__header selected';
-    document.querySelector('.portfolio__sections .selector').className = `selector selector__${this.dataset.section}`;
+    // document.querySelector('.portfolio__sections .selector').className = `selector selector__${this.dataset.section}`;
     // change section elements
     document.querySelector(`#${prev.dataset.section}`).classList.add('hide');
     document.querySelector(`#${this.dataset.section}`).classList.remove('hide');
