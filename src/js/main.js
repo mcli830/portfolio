131b2067bfa2c84e44f0d10c1e2eae378245a1c6
mcli830 <39263631+mcli830@users.noreply.github.com>
2019-06-01 19:17:56 -0700

    add particles bg behind namecard

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 // IMPORTS
 // const initParticles = require('./particlesBG.js')
+const initParticles = require('./particlesBG.js')
 const initFilterFunctions = require('./filter.js')
 const initSectionButtons = require('./sections.js')
 const initNamecardFunctions = require('./namecard.js')
@@ -13,6 +14,7 @@ const animations = {}
 // DOM CONTENT LOADED
 document.addEventListener('DOMContentLoaded', init)
 function init(){
+  initParticles();
   initAnimations();
   initSectionButtons(animations);
   initFilterFunctions(state);
--- src/js/particlesBG.js
+++ src/js/particlesBG.js
@@ -1,10 +1,12 @@
 const Particles = require('particlesjs')
 
-module.exports = Particles.init({
-  selector: '#particles',
-  maxParticles: 150,
-  speed: 0.25,
-  color: '#67daff',
-  minDistance: 30,
-  connectParticles: true
-});
+module.exports = function(){
+  Particles.init({
+    selector: '#particles',
+    maxParticles: 150,
+    speed: 0.25,
+    color: '#67daff',
+    minDistance: 30,
+    connectParticles: true
+  });
+}
